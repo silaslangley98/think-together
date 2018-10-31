f8254a1d34ca40cc44b711f26a9a516f67a3dbed
Silas Langley <silaslangley98@gmail.com>
2018-10-30 17:13:52 -0700

    feat(api): move api service

--- src/app/question-answer/shared/question.service.ts
+++ src/app/question-answer/shared/question.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { ApiService } from '../../services/api.service';
+import { ApiService } from '../../shared/services/api.service';
 
 @Injectable({
 	providedIn: 'root',
