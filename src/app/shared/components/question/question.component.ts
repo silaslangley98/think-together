4682f21738ab741bca15cd12dd40cc93f332e807
Silas Langley <silaslangley98@gmail.com>
2018-11-09 16:51:48 -0800

    feat(question): add question component

---
+++ src/app/shared/components/question/question.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Question } from '../../../question-answer/shared/question';
+
+@Component({
+	selector: 'app-question',
+	templateUrl: './question.component.html',
+	styleUrls: ['./question.component.scss'],
+})
+export class QuestionComponent implements OnInit {
+	@Input() question: Question;
+
+	constructor() { }
+
+	ngOnInit() {
+		console.log(this.question);
+	}
+}
