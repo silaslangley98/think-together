611cb85612b5273ac26dfc61d5bb4d2947b65627
Silas Langley <silaslangley98@gmail.com>
2018-11-17 17:15:33 -0800

    feat(add-question-dialog): add save and cancel methods

--- src/app/shared/components/add-question-dialog/add-question-dialog.component.ts
+++ src/app/shared/components/add-question-dialog/add-question-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef } from "@angular/material";
 
 @Component({
 	selector    : 'app-add-question-dialog',
@@ -7,12 +8,22 @@ import { FormControl, FormGroup } from '@angular/forms';
 	styleUrls   : ['./add-question-dialog.component.scss']
 })
 export default class AddQuestionDialogComponent implements OnInit {
-	constructor() { }
+	constructor(
+		private dialogRef : MatDialogRef<AddQuestionDialogComponent>
+	) { }
 
 	addQuestionForm = new FormGroup({
 		question : new FormControl(''),
 	});
 
 	ngOnInit() {
+	}
+
+	cancel() {
+		this.dialogRef.close();
+	}
+
+	save() {
+		this.dialogRef.close(this.addQuestionForm.value);
 	}
 }
