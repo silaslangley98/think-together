2408f324e9bbc158eed2bd9fe70efa9047f03911
Silas Langley <silaslangley98@gmail.com>
2018-12-03 16:48:39 -0800

    feat(question-answer): display answers

--- src/app/question-answer/question-detail/question-detail.component.ts
+++ src/app/question-answer/question-detail/question-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { QuestionService } from '../shared/question.service';
+import { AnswerService } from '../shared/answer.service';
 
 import { Question } from '../shared/question';
 
@@ -11,14 +13,27 @@ import { Question } from '../shared/question';
 })
 export class QuestionDetailComponent implements OnInit {
 	question: Question;
+	public answers$: Observable<any[]>;
 
-	constructor(private questionService: QuestionService) { }
+	constructor(
+		private questionService: QuestionService,
+		private answerService: AnswerService,
+	) { }
 
 	ngOnInit() {
 		this.getQuestion();
+		this.getAnswers();
 	}
 
 	getQuestion():void {
 		this.question = this.questionService.getQuestion();
+	}
+
+	getAnswers():void {
+		this.answers$ = this.answerService.getAnswers(this.question.id);
+	}
+
+	trackByAnswerId({}, answer: any): string {
+		return answer.id;
 	}
 }
