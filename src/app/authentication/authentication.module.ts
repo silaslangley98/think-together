122eb4a02e91540a2d3c5c37e92a801d44554c7d
Silas Langley <silaslangley98@gmail.com>
2019-05-09 16:13:44 -0700

    feat(authentication): import modules

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { routing } from './authentication-routing.module';
+
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
 	imports: [
-		CommonModule
+		CommonModule,
+		FlexLayoutModule,
+		MaterialModule,
+		ReactiveFormsModule,
+		routing,
 	],
 	declarations: [LoginComponent, SignupComponent],
 })
