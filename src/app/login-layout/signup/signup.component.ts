045f764aca0fb0f0866cf43de4e82be7fb97b9c1
Silas Langley <silaslangley98@gmail.com>
2019-05-11 17:23:08 -0700

    feature(signup): add signup component

---
+++ src/app/login-layout/signup/signup.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { AuthService } from '../../authentication/auth.service';
+
+@Component({
+	selector    : 'app-signup',
+	templateUrl : './signup.component.html',
+	styleUrls   : ['./signup.component.scss']
+})
+
+export class SignupComponent implements OnInit {
+
+	constructor(private auth: AuthService) { }
+
+	signupForm = new FormGroup({
+		name            : new FormControl(''),
+		email           : new FormControl(''),
+		password        : new FormControl(''),
+		confirmPassword : new FormControl(''),
+	});
+
+	ngOnInit() {
+	}
+
+	signup() {
+		const { confirmPassword, name, ...signupCredentials } = this.signupForm.value;
+
+		this.auth.signup(signupCredentials, name);
+	}
+}
